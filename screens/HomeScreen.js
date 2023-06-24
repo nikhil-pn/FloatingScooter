ca46e7fce0b14a5bbabeb06c2b047a7a5cf04251
nikhilpn725 <nikhilpn725@gmail.com>
2023-06-24 19:35:27 +0530

    GooglePlacesAutoComplete comp created!

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,9 +2,13 @@ import { View, Text, SafeAreaView, Image } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { selectDestination,  setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
-    
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={tw`bg-white h-full `}>
       <View style={tw`p-5`}>
@@ -12,6 +16,36 @@ const HomeScreen = () => {
           style={{ width: 100, height: 100, resizeMode: "contain" }}
           source={require("../assets/metroCycleLogo.png")}
         ></Image>
+
+        <GooglePlacesAutocomplete
+          placeholder="Where From?"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            TextInput: {
+              fontSize: 18,
+            },
+          }}
+          onpress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+          }}
+          fetchDetails={true}
+          returnKeyType={"Search"}
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debouce={400}
+        />
         <NavOptions></NavOptions>
       </View>
     </SafeAreaView>
