66abc5a1c6876ea10e3cc6d272f16f155c6a225b
nikhilpn725 <nikhilpn725@gmail.com>
2023-06-24 16:19:15 +0530

    redux store implemented - navSlice, store.js provider-app

---
+++ store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+// import dataReducer from './slices/dataSlice' 
+import navReducer from './slices/navSlice' 
+
+export const store = configureStore({
+    reducer: {
+        nav: navReducer
+    }
+});
